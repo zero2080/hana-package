4f9c77dfb1d0827fcc3e39ccd96d84844baee311
peachy-dino <dino@peachy.run>
2022-02-06 01:50:01 +0900

    api path

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,5 @@
-const API_PATH = 'http://localhost:8080';
-// const API_PATH = 'http://hanapackageeb-env.eba-sduwzjf3.ap-northeast-2.elasticbeanstalk.com';
+// const API_PATH = 'http://localhost:8080';
+const API_PATH = 'http://hanapackageeb-env.eba-sduwzjf3.ap-northeast-2.elasticbeanstalk.com';
 
 function request(option){
     return fetch(API_PATH+option.url,{
